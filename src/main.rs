8708fb0486fd27693bcf69f6ae470207d35e3ec8
Brian Martin <bmartin@twitter.com>
2022-03-29 15:19:11 -0700

    improve main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::hash::Hasher;
+use std::hash::BuildHasher;
+use ahash::RandomState;
+
 fn main() {
-    println!("use `cargo bench` to run")
-    
+    let hash_builder = RandomState::with_seeds(
+        0xbb8c484891ec6c86,
+        0x0522a25ae9c769f9,
+        0xeed2797b9571bc75,
+        0x4feb29c1fbbd59d0,
+    );
+
+    let mut hasher = hash_builder.build_hasher();
+    hasher.write_u64(0);
+    let v = hasher.finish();
+    println!("hash: {}", v);
 }
